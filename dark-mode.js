79aa98a32f6dd70a9649c792113ac9047bbea559
heriwr <159926288+heriwr@users.noreply.github.com>
2024-03-01 23:05:15 +0000

    Update dark-mode.js

--- dark-mode.js
+++ dark-mode.js
@@ -1,11 +1,11 @@
 const toggle = document.getElementById('dark-mode-toggle');
 
 toggle.addEventListener('change', () => {
-  document.body.classList.toggle('dark-mode'); // Use body instead of the root 
+  document.body.classList.toggle('dark-mode');
   localStorage.setItem('darkMode', document.body.classList.contains('dark-mode'));
 });
 
-// Initial dark mode setup on page load
+// Load preference on initial page visit
 if (localStorage.getItem('darkMode') === 'true') {
   document.body.classList.add('dark-mode');
   toggle.checked = true;
